68bf047a3b6921eaf56678ca4e95b6ab1fe710e1
Jeffrey Miller <mill7707@sputnik.morris.umn.edu>
2021-09-11 15:02:37 -0500

    Moves file to desired directory

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,3 +6,5 @@ cd "$1"/var/log || exit
 cat ./* > file_to_process.txt
 
 awk 'match($0, /([A-Z]\w+) (\d\d) (\d\d):\d\d:\d\d [A-Za-z]\w+ sshd\[\d\d\d\d\d\]: Failed password for (?:invalid user )?([a-zA-Z]\w+) from (\d+.\d+.\d+.\d+) port \d+ ssh2/, groups) {print groups[1] groups[2] groups[3] groups[4] groups[5] }' file_to_process.txt > failed_login_data.txt
+
+mv failed_login_data.txt ../../
