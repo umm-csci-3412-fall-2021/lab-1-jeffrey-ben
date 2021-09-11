92238abb3f2d0c54b44b5b3ff651cceda0bd8ba0
Jeffrey Miller <mill7707@sputnik.morris.umn.edu>
2021-09-11 14:16:19 -0500

    Adds wrap_contents.sh file as well as testing file for awk. Modifies process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,4 +2,11 @@
 
 cd $1 || exit
 
-cat "$log/*.*" | grep -of "$log_regex" > "failed_login_data.txt"
+cd var || exit
+
+cd log || exit
+
+
+cat * > file_to_process.txt
+
+awk 'match($0, /([A-Z]\w+) (\d\d) (\d\d):\d\d:\d\d [A-Za-z]\w+ sshd\[\d\d\d\d\d\]: Failed password for (?:invalid user )?([a-zA-Z]\w+) from (\d+.\d+.\d+.\d+) port \d+ ssh2/, groups) {print groups[1] groups[2] groups[3] groups[4] groups[5] }' file_to_process.txt > failed_login_data.txt
