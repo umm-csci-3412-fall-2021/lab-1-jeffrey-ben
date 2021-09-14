55865446520265b2224befa68fd169dc9fbcd5d1
Jeffrey Miller <mill7707@sputnik.morris.umn.edu>
2021-09-14 15:28:31 -0500

    Fixes the wrapper script, completes the create_username_dist.sh script.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+
+beginningdir="$(pwd)"
 temp_file="$(mktemp /tmp/temp_file.XXXXXXXXX)"
 startd="$(pwd)"
 
@@ -16,7 +18,7 @@ cat */failed_login_data.txt | awk '{ print $4 }' | sort | uniq -c | awk '{ print
 
 
 
-cd .. || exit
+cd "$startd"	 || exit
 
 #Run wrap contents
 ./bin/wrap_contents.sh "$temp_file" html_components/username_dist "$1"/username_dist.html
--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -2,7 +2,7 @@
 
 var_middle=$1
 
-var_footer=$(find . -name "$2*_footer*")
-var_header=$(find . -name "$2*_header*")
+var_footer="$2_footer.html"
+var_header="$2_header.html"
 
 cat "$var_header" "$var_middle" "$var_footer" > "$3"
