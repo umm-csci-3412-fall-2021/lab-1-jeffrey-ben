0ecb38b5b6ffc8276a237288903fcf3d06346c62
Jeffrey Miller <mill7707@bistromath.morris.umn.edu>
2021-09-15 18:26:46 -0500

    Adds comments

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -3,15 +3,25 @@
 temp_file="$(mktemp /tmp/temp_file.XXXXXXXXX)"
 startd="$(pwd)"
 
+#Goes into arguement file
 cd "$1" || exit
 
-
+#Concatenates all "failed_login_txt" files into a single text file
+#Prints out the 3th column, it being the hour field
+#Sorts it
+#Arranges them in order of how many occur in file
+#Uses awk to print the 1st and 2nd column into the html format
+#Pushes it into the temp file
 cat ./*/failed_login_data.txt | awk '{ print $3 }' | sort | uniq -c | awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);"}' > "$temp_file"
+
+
 
 cd "$startd" || exit
 
-
+#Run wrap contents
 ./bin/wrap_contents.sh "$temp_file" html_components/hours_dist "$1"/hours_dist.html
 
+
+#Remove temp file
 rm "$temp_file"
 
