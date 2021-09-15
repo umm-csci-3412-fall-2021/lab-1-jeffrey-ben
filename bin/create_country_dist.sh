201cfa79232892a1aa0757786ddafcd6e7321d0e
Jeffrey Miller <mill7707@bistromath.morris.umn.edu>
2021-09-15 18:38:53 -0500

    Creates basic outline for create_country_dist.sh and makes it executable

---
+++ bin/create_country_dist.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+
+
+
+temp_file="$(mktemp /tmp/temp_file.XXXXXXXXX)"
+startd="$(pwd)"
+
+#Goes into arguement file
+cd "$1" || exit
+
+#Concatenates all "failed_login_txt" files into a single text file
+#Prints out the 5th column, it being the IP field
+#Sorts it
+#Arranges them in order of how many occur in file
+#Uses awk to print the 1st and 2nd column into the html format
+#Pushes it into the temp file
+cat ./*/failed_login_data.txt | awk '{ print $4 }' | sort | uniq -c | awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);"}' > "$temp_file"
+
+
+
+cd "$startd"     || exit
+
+#Run wrap contents
+./bin/wrap_contents.sh "$temp_file" html_components/country_dist "$1"/country_dist.html
+
+
+#Remove temp file
+rm "$temp_file"
+
